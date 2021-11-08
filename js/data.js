ae9a442f5e04f7a60fb02d0c51e7fb7624228c42
muborizMirzoev <muboriz10@gmail.com>
2021-11-09 04:07:24 +0700

    feat: add quiz layout

---
+++ js/data.js
@@ -0,0 +1,104 @@
+export const data = [
+   {
+      question: 'JavaScript это тоже самое что и Java?',
+      answer: [
+         {
+            value: 'Нет.',
+            status: true
+         },
+         {
+            value: 'Да.',
+            status: false
+         },
+         {
+            value: 'Когда как.',
+            status: false
+         }
+      ]
+   },
+   {
+      question: 'Сколько параметров можно передавать в функцию?',
+      answer: [
+         {
+            value: 'Только столько, сколько указано при ее созданию.',
+            status: false
+         },
+         {
+            value: 'Минимум 1..',
+            status: false
+         },
+         {
+            value: 'Сколько угодно.',
+            status: true
+         }
+      ]
+   },
+   {
+      question: 'Что такое: "use strict"?',
+      answer: [
+         {
+            value: 'Просто строка',
+            status: false
+         },
+         {
+            value: 'Функция.',
+            status: false
+         },
+         {
+            value: 'Строгий режим.',
+            status: true
+         }
+      ]
+   },
+   {
+      question: '4',
+      answer: [
+         {
+            value: 'Просто строка',
+            status: false
+         },
+         {
+            value: 'Функция.',
+            status: false
+         },
+         {
+            value: 'Строгий режим.',
+            status: true
+         }
+      ]
+   },
+   {
+      question: '5',
+      answer: [
+         {
+            value: 'Просто строка',
+            status: false
+         },
+         {
+            value: 'Функция.',
+            status: false
+         },
+         {
+            value: 'Строгий режим.',
+            status: true
+         }
+      ]
+   },
+   {
+      question: '6',
+      answer: [
+         {
+            value: 'Просто строка',
+            status: false
+         },
+         {
+            value: 'Функция.',
+            status: false
+         },
+         {
+            value: 'Строгий режим.',
+            status: true
+         }
+      ]
+   }
+];
